225247cbffd251f585ed3d03e3f8916fa7890508
JuliandroR <juliandro.rocha@gmail.com>
2020-01-17 07:44:59 -0400

    Initial Commit

---
+++ precache-manifest.aeb6a4872d48edc6756f2ec51e1b232f.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "f670ada487624393631a",
+    "url": "/css/app.2080100a.css"
+  },
+  {
+    "revision": "f670ada487624393631a",
+    "url": "/js/app.7e0046d5.js"
+  },
+  {
+    "revision": "b294a50c7dd42e5cf454",
+    "url": "/js/chunk-vendors.79220dca.js"
+  },
+  {
+    "revision": "e9e867de1ec26b13ecc231769aa4a948",
+    "url": "/img/cifra-vigenere.e9e867de.png"
+  },
+  {
+    "revision": "56ebdbcddf4b38ebcf455eb0c6eb96e3",
+    "url": "/img/lucimara.56ebdbcd.png"
+  },
+  {
+    "revision": "120f58218acc4d67dd2d005e375e7dd8",
+    "url": "/img/page-ieducar.120f5821.png"
+  },
+  {
+    "revision": "b7a613bfc1cf76a646de78adf3d8753a",
+    "url": "/img/cordova.b7a613bf.png"
+  },
+  {
+    "revision": "8c9c1ca256f0ee29d76911d849107eaa",
+    "url": "/img/calculadora-ip.8c9c1ca2.png"
+  },
+  {
+    "revision": "adcc4a67179f7a949ce4d78d3a5f0acb",
+    "url": "/img/assistance.adcc4a67.png"
+  },
+  {
+    "revision": "c48e508b9ec3de8bdf68a834f43d45f9",
+    "url": "/img/62.c48e508b.jpg"
+  },
+  {
+    "revision": "470bd763d61241a5c4cd92d3d15a361c",
+    "url": "/img/logo_white.470bd763.png"
+  },
+  {
+    "revision": "fb3147f28d746cf240eebded191bd68a",
+    "url": "/img/javascript.fb3147f2.png"
+  },
+  {
+    "revision": "33503a57cafa8c716fd8f8e9805d8241",
+    "url": "/img/github.33503a57.png"
+  },
+  {
+    "revision": "eb7f03896bfe34c7cdd544465c7d0928",
+    "url": "/img/background.eb7f0389.png"
+  },
+  {
+    "revision": "f73c6b663d88eed91bd451cb2445cd0f",
+    "url": "/fonts/Viga.f73c6b66.woff2"
+  },
+  {
+    "revision": "40c38faf9d4cde6e91bc021f68571606",
+    "url": "/fonts/CreateRound.40c38faf.woff2"
+  },
+  {
+    "revision": "1a280523d375e9358d5229df34fc8e94",
+    "url": "/fonts/Poppins.1a280523.woff2"
+  },
+  {
+    "revision": "5a23b2ada9413b2ec9772c67d29cca12",
+    "url": "/img/profile_foto_no_background.5a23b2ad.png"
+  },
+  {
+    "revision": "57ee7d9832b001db0c588379df66bf71",
+    "url": "/img/profile_foto.57ee7d98.png"
+  },
+  {
+    "revision": "651cfe6121f67820526facce7dc18cc7",
+    "url": "/img/speak.651cfe61.png"
+  },
+  {
+    "revision": "4c9b83092326c132a591da36b65971aa",
+    "url": "/img/artes-graficas.4c9b8309.png"
+  },
+  {
+    "revision": "56d177764c84c8d2a3044d5c1f58be53",
+    "url": "/img/bit-journal.56d17776.png"
+  },
+  {
+    "revision": "3ade4221bdc06681068f5b5b3c71692e",
+    "url": "/img/nodejs.3ade4221.png"
+  },
+  {
+    "revision": "4f31b3344f8f6bc463b3ee0a57910696",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
